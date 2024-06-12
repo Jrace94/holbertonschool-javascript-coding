b2890168b17244c2565852d73b76fb792e7d4e74
Jrace94 <jfra.ing28@gmail.com>
2024-06-11 20:17:44 -0500

    Task 8: Right order

--- javascript-web_scraping/101-starwars_characters.js
+++ javascript-web_scraping/101-starwars_characters.js
@@ -5,6 +5,7 @@
 */
 const request = require('request');
 const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+
 request(url, function (error, response, body) {
   if (error) console.log(error);
   else {
